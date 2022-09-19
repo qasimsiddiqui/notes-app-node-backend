d74a77ec32ef0dff9a77617b060b587ea3ac4986
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-09-19 11:05:44 +0500

    user routes added

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
 import express, { Application, Request, Response } from "express";
-const cors = require( "cors");
 
-const app:Application = express();
+const cors = require("cors");
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/v1/api/", async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-        message: "Hello World!",
-    });
+  return res.status(200).send({
+    message: "Hello World!",
+  });
 });
 
-app.use('/v1/api/', require('./routes/index'))
+app.use("/v1/api/", require("./routes/index"));
+app.use("/v1/api/", require("./routes/user"));
 
 try {
-    app.listen(4000, (): void => {
-        console.log(`Connected successfully on port ${4000}`);
-    });
+  app.listen(4000, (): void => {
+    console.log(`Connected successfully on port ${4000}`);
+  });
 } catch (error: any) {
-    console.error(`Error occured: ${error.message}`);
+  console.error(`Error occurred: ${error.message}`);
 }
