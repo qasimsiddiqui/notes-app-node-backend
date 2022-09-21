75b53bbda459d5ef8584dd7fbeb339a8ac7d251d
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-09-21 12:01:46 +0500

    add authentication middleware to users route

--- src/users/userRoutes.ts
+++ src/users/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import usersController from "./controller/usersController";
+import { middleware } from "../utils/authService";
 
 const router: Router = Router();
 
 export function usersRoutes(): Router {
-  router.route("/users").get(usersController.getAll);
+  router.route("/users").get(middleware, usersController.getAll);
   router.route("/user").put(usersController.create);
   return router;
 }
