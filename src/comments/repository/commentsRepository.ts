4b03715d5b378f5f0901383077f4079f62f84263
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-09-26 11:00:26 +0500

    id field name changed to comment_id

--- src/comments/repository/commentsRepository.ts
+++ src/comments/repository/commentsRepository.ts
@@ -11,94 +11,94 @@ const getAll = async (noteID: string) => {
 };
 
 const addComment = async (
-    noteID: string,
-    userID: string,
-    userName: string,
-    content: string
+  noteID: string,
+  userID: string,
+  userName: string,
+  content: string
 ) => {
-    const documentReference = await db
-        .collection(`notes/${noteID}/comments`)
-        .doc();
-    return await documentReference
-        .set({
-            id: documentReference.id,
-            content,
-            author_id: userID,
-            author_name: userName,
-            time_created: Date.now(),
-            time_updated: Date.now(),
-        })
-        .then(() => {
-            return {message: "New Comment added"};
-        })
-        .catch((err) => {
-            throw new Error(err.message);
-        });
+  const documentReference = await db
+    .collection(`notes/${noteID}/comments`)
+    .doc();
+  return await documentReference
+    .set({
+      comment_id: documentReference.id,
+      content,
+      author_id: userID,
+      author_name: userName,
+      time_created: Date.now(),
+      time_updated: Date.now(),
+    })
+    .then(() => {
+      return { message: "New Comment added" };
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 };
 
 const deleteComment = async (
-    noteID: string,
-    commentID: string,
-    userID: string
+  noteID: string,
+  commentID: string,
+  userID: string
 ) => {
-    let doc: any = await db
-        .collection(`notes/${noteID}/comments`)
-        .doc(commentID)
-        .get();
+  let doc: any = await db
+    .collection(`notes/${noteID}/comments`)
+    .doc(commentID)
+    .get();
 
-    doc = doc.data();
+  doc = doc.data();
 
-    if (doc.author_id === userID) {
-        return await db
-            .collection(`notes/${noteID}/comments`)
-            .doc(commentID)
-            .delete()
-            .then(() => {
-                return {message: "Comment successfully deleted"};
-            })
-            .catch((err) => {
-                throw new Error(err.message);
-            });
-    } else {
-        return {message: "Comment not created by you."};
-    }
+  if (doc.author_id === userID) {
+    return await db
+      .collection(`notes/${noteID}/comments`)
+      .doc(commentID)
+      .delete()
+      .then(() => {
+        return { message: "Comment successfully deleted" };
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+  } else {
+    return { message: "Comment not created by you." };
+  }
 };
 
 const updateComment = async (
-    noteID: string,
-    commentID: string,
-    userID: string,
-    content: string
+  noteID: string,
+  commentID: string,
+  userID: string,
+  content: string
 ) => {
-    let doc: any = await db
-        .collection(`notes/${noteID}/comments`)
-        .doc(commentID)
-        .get();
+  let doc: any = await db
+    .collection(`notes/${noteID}/comments`)
+    .doc(commentID)
+    .get();
 
-    doc = doc.data();
+  doc = doc.data();
 
-    if (doc.author_id === userID) {
-        return await db
-            .collection(`notes/${noteID}/comments`)
-            .doc(commentID)
-            .update({
-                content,
-                time_updated: Date.now()
-            })
-            .then(() => {
-                return {message: "Comment successfully updated"};
-            })
-            .catch((err) => {
-                throw new Error(err.message);
-            });
-    } else {
-        return {message: "Comment not created by you."};
-    }
+  if (doc.author_id === userID) {
+    return await db
+      .collection(`notes/${noteID}/comments`)
+      .doc(commentID)
+      .update({
+        content,
+        time_updated: Date.now(),
+      })
+      .then(() => {
+        return { message: "Comment successfully updated" };
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+  } else {
+    return { message: "Comment not created by you." };
+  }
 };
 
 export default {
-    getAll,
-    addComment,
-    deleteComment,
-    updateComment,
+  getAll,
+  addComment,
+  deleteComment,
+  updateComment,
 };
