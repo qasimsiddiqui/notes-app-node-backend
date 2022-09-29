e3c972cfe6a81f76b05a4e7b30566e80b6c2d908
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-09-29 12:52:06 +0500

    NotificationModel added

---
+++ src/notifications/model/notification.model.ts
@@ -0,0 +1,9 @@
+interface NotificationInterface {
+  notification_id: string;
+  message: string;
+  type: string;
+  note_id: string;
+  isRead: boolean;
+  time_created: number;
+  time_read: number;
+}
