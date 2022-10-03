2b6b1443574085f57c8db5ff24b0d14d929edb0c
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-10-03 11:03:10 +0500

    CommentsInterface added with JsDoc

---
+++ src/comments/model/comment.model.ts
@@ -0,0 +1,9 @@
+export interface CommentInterface {
+  author_id: string;
+  author_name: string;
+  comment_id: string;
+  content: string;
+  isEdited: boolean;
+  time_created: number;
+  time_updated: number;
+}
