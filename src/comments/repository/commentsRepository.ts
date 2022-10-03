5d2ca41bf0381f6a1c159c4e8399647c25cbb115
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-10-03 11:22:11 +0500

    commentsRepository and commentService changed to classes

--- src/comments/repository/commentsRepository.ts
+++ src/comments/repository/commentsRepository.ts
@@ -8,145 +8,142 @@ import db from "../../firebase";
 import * as NotificationRepository from "../../notifications/repository/notifications.repository";
 import { CommentInterface } from "../model/comment.model";
 
-/**
- * Get all comments for a note
- * @param {string} noteID Note ID
- * @returns {Promise<CommentInterface[]>}
- */
-const getAll = async (noteID: string) => {
-  const querySnapshot: QuerySnapshot = await db
-    .collection(`notes/${noteID}/comments`)
-    .orderBy("time_created")
-    .get();
-  return querySnapshot.docs.map(
-    (doc: QueryDocumentSnapshot): CommentInterface => {
-      return doc.data() as CommentInterface;
-    }
-  );
-};
-
-/**
- * Add a comment to a note
- * @param noteID Note ID
- * @param noteAuthorID Note author user ID
- * @param userID User ID
- * @param userName User name
- * @param content Comment body
- * @returns {Promise<any>}
- */
-const addComment = async (
-  noteID: string,
-  noteAuthorID: string,
-  userID: string,
-  userName: string,
-  content: string
-) => {
-  try {
-    const commentDocRef: DocumentReference = await db
+class NotesRepository {
+  /**
+   * Get all comments for a note
+   * @param {string} noteID Note ID
+   * @returns {Promise<CommentInterface[]>}
+   */
+  async getAll(noteID: string): Promise<CommentInterface[]> {
+    const querySnapshot: QuerySnapshot = await db
       .collection(`notes/${noteID}/comments`)
-      .doc();
-    await commentDocRef.set({
-      comment_id: commentDocRef.id,
-      content,
-      author_id: userID,
-      author_name: userName,
-      isEdited: false,
-      time_created: Date.now(),
-      time_updated: Date.now(),
-    });
-
-    if (noteAuthorID !== userID) {
-      await NotificationRepository.addCommentNotification(
-        noteID,
-        noteAuthorID,
-        userName
-      );
-    }
-    return { message: "Comment successfully added" };
-  } catch (error: any) {
-    throw new Error(error.message);
+      .orderBy("time_created")
+      .get();
+    return querySnapshot.docs.map(
+      (doc: QueryDocumentSnapshot): CommentInterface => {
+        return doc.data() as CommentInterface;
+      }
+    );
   }
-};
 
-/**
- * Delete a comment
- * @param noteID  Note ID
- * @param commentID Comment ID
- * @param userID User ID
- * @returns {Promise<any>}
- */
-const deleteComment = async (
-  noteID: string,
-  commentID: string,
-  userID: string
-) => {
-  const doc: DocumentData = await db
-    .collection(`notes/${noteID}/comments`)
-    .doc(commentID)
-    .get();
-
-  const comment: CommentInterface = doc.data() as CommentInterface;
-
-  if (comment.author_id === userID) {
-    return await db
-      .collection(`notes/${noteID}/comments`)
-      .doc(commentID)
-      .delete()
-      .then(() => {
-        return { message: "Comment successfully deleted" };
-      })
-      .catch((err) => {
-        throw new Error(err.message);
-      });
-  } else {
-    return { message: "Comment not created by you." };
-  }
-};
-
-/**
- * Update a comment
- * @param noteID Note ID
- * @param commentID Comment ID
- * @param userID User ID
- * @param content Comment body
- * @returns {Promise<any>}
- */
-const updateComment = async (
-  noteID: string,
-  commentID: string,
-  userID: string,
-  content: string
-) => {
-  const doc: DocumentData = await db
-    .collection(`notes/${noteID}/comments`)
-    .doc(commentID)
-    .get();
-
-  const comment: CommentInterface = doc.data() as CommentInterface;
-
-  if (comment.author_id === userID) {
-    return await db
-      .collection(`notes/${noteID}/comments`)
-      .doc(commentID)
-      .update({
+  /**
+   * Add a comment to a note
+   * @param noteID Note ID
+   * @param noteAuthorID Note author user ID
+   * @param userID User ID
+   * @param userName User name
+   * @param content Comment body
+   * @returns {Promise<any>}
+   */
+  async addComment(
+    noteID: string,
+    noteAuthorID: string,
+    userID: string,
+    userName: string,
+    content: string
+  ): Promise<any> {
+    try {
+      const commentDocRef: DocumentReference = await db
+        .collection(`notes/${noteID}/comments`)
+        .doc();
+      await commentDocRef.set({
+        comment_id: commentDocRef.id,
         content,
-        isEdited: true,
+        author_id: userID,
+        author_name: userName,
+        isEdited: false,
+        time_created: Date.now(),
         time_updated: Date.now(),
-      })
-      .then(() => {
-        return { message: "Comment successfully updated" };
-      })
-      .catch((err) => {
-        throw new Error(err.message);
       });
-  } else {
-    return { message: "Comment not created by you." };
-  }
-};
 
-export default {
-  getAll,
-  addComment,
-  deleteComment,
-  updateComment,
-};
+      if (noteAuthorID !== userID) {
+        await NotificationRepository.addCommentNotification(
+          noteID,
+          noteAuthorID,
+          userName
+        );
+      }
+      return { message: "Comment successfully added" };
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  /**
+   * Delete a comment
+   * @param noteID  Note ID
+   * @param commentID Comment ID
+   * @param userID User ID
+   * @returns {Promise<any>}
+   */
+  async deleteComment(
+    noteID: string,
+    commentID: string,
+    userID: string
+  ): Promise<any> {
+    const doc: DocumentData = await db
+      .collection(`notes/${noteID}/comments`)
+      .doc(commentID)
+      .get();
+
+    const comment: CommentInterface = doc.data() as CommentInterface;
+
+    if (comment.author_id === userID) {
+      return await db
+        .collection(`notes/${noteID}/comments`)
+        .doc(commentID)
+        .delete()
+        .then(() => {
+          return { message: "Comment successfully deleted" };
+        })
+        .catch((err) => {
+          throw new Error(err.message);
+        });
+    } else {
+      return { message: "Comment not created by you." };
+    }
+  }
+
+  /**
+   * Update a comment
+   * @param noteID Note ID
+   * @param commentID Comment ID
+   * @param userID User ID
+   * @param content Comment body
+   * @returns {Promise<any>}
+   */
+  async updateComment(
+    noteID: string,
+    commentID: string,
+    userID: string,
+    content: string
+  ): Promise<any> {
+    const doc: DocumentData = await db
+      .collection(`notes/${noteID}/comments`)
+      .doc(commentID)
+      .get();
+
+    const comment: CommentInterface = doc.data() as CommentInterface;
+
+    if (comment.author_id === userID) {
+      return await db
+        .collection(`notes/${noteID}/comments`)
+        .doc(commentID)
+        .update({
+          content,
+          isEdited: true,
+          time_updated: Date.now(),
+        })
+        .then(() => {
+          return { message: "Comment successfully updated" };
+        })
+        .catch((err) => {
+          throw new Error(err.message);
+        });
+    } else {
+      return { message: "Comment not created by you." };
+    }
+  }
+}
+
+export default new NotesRepository();
--- src/comments/service/commentService.ts
+++ src/comments/service/commentService.ts
@@ -1,95 +1,92 @@
 import CommentsRepository from "../repository/commentsRepository";
 
-/**
- * Get all comments for a note
- * @param {string} noteID Note ID
- * @returns {Promise<any>}
- */
-const getAll = async (noteID: string): Promise<any> => {
-  try {
-    return await CommentsRepository.getAll(noteID);
-  } catch (e: any) {
-    throw new Error(e.message);
+class CommentService {
+  /**
+   * Get all comments for a note
+   * @param {string} noteID Note ID
+   * @returns {Promise<any>}
+   */
+  async getAll(noteID: string): Promise<any> {
+    try {
+      return await CommentsRepository.getAll(noteID);
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
   }
-};
 
-/**
- * Add a comment to a note
- * @param {sring} noteID Note ID
- * @param {sring} noteAuthorID Note author ID
- * @param {sring} userID User ID
- * @param {sring} userName User name
- * @param {sring} content Comment body
- * @returns {Promise<any>}
- */
-const addComment = async (
-  noteID: string,
-  noteAuthorID: string,
-  userID: string,
-  userName: string,
-  content: string
-): Promise<any> => {
-  try {
-    return await CommentsRepository.addComment(
-      noteID,
-      noteAuthorID,
-      userID,
-      userName,
-      content
-    );
-  } catch (e: any) {
-    throw new Error(e.message);
+  /**
+   * Add a comment to a note
+   * @param {sring} noteID Note ID
+   * @param {sring} noteAuthorID Note author ID
+   * @param {sring} userID User ID
+   * @param {sring} userName User name
+   * @param {sring} content Comment body
+   * @returns {Promise<any>}
+   */
+  async addComment(
+    noteID: string,
+    noteAuthorID: string,
+    userID: string,
+    userName: string,
+    content: string
+  ): Promise<any> {
+    try {
+      return await CommentsRepository.addComment(
+        noteID,
+        noteAuthorID,
+        userID,
+        userName,
+        content
+      );
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
   }
-};
 
-/**
- * Delete a comment
- * @param {string} noteID Note ID
- * @param {string} commentID Comment ID
- * @param {string} userID User ID
- * @returns {Promise<any>}
- */
-const deleteComment = async (
-  noteID: string,
-  commentID: string,
-  userID: string
-): Promise<any> => {
-  try {
-    return await CommentsRepository.deleteComment(noteID, commentID, userID);
-  } catch (e: any) {
-    throw new Error(e.message);
+  /**
+   * Delete a comment
+   * @param {string} noteID Note ID
+   * @param {string} commentID Comment ID
+   * @param {string} userID User ID
+   * @returns {Promise<any>}
+   */
+  async deleteComment(
+    noteID: string,
+    commentID: string,
+    userID: string
+  ): Promise<any> {
+    try {
+      return await CommentsRepository.deleteComment(noteID, commentID, userID);
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
   }
-};
 
-/**
- * Update a comment
- * @param {string} noteID Note ID
- * @param {string} commentID Comment ID
- * @param {string} userID User ID
- * @param {string} content Comment body
- * @returns {Promise<any>}
- */
-const updateComment = async (
-  noteID: string,
-  commentID: string,
-  userID: string,
-  content: string
-): Promise<any> => {
-  try {
-    return await CommentsRepository.updateComment(
-      noteID,
-      commentID,
-      userID,
-      content
-    );
-  } catch (e: any) {
-    throw new Error(e.message);
+  /**
+   * Update a comment
+   * @param {string} noteID Note ID
+   * @param {string} commentID Comment ID
+   * @param {string} userID User ID
+   * @param {string} content Comment body
+   * @returns {Promise<any>}
+   */
+  async updateComment(
+    noteID: string,
+    commentID: string,
+    userID: string,
+    content: string
+  ): Promise<any> {
+    try {
+      return await CommentsRepository.updateComment(
+        noteID,
+        commentID,
+        userID,
+        content
+      );
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
   }
-};
+}
 
-export default {
-  getAll,
-  addComment,
-  deleteComment,
-  updateComment,
-};
+export default new CommentService();
