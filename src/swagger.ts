a96d671500891ef68fbf5d99cb5327204194ad1e
Qasim Siddiqui <qasim.siddiqui678@gmail.com>
2022-10-26 10:50:54 +0500

    added types and secuirty in swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,6 @@
 import swagger from "swagger-jsdoc";
 
-const swaggerDefinition = {
+const swaggerDefinition: swagger.OAS3Definition = {
   openapi: "3.0.0",
   info: {
     title: "Notes app API with Swagger",
@@ -9,16 +9,26 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:4000",
+      url: "http://localhost:4000/v1/api",
       description: "Development server",
+    },
+  ],
+
+  security: [
+    {
+      bearerAuth: [],
     },
   ],
 };
 
-const options = {
-  swaggerDefinition,
+const options: swagger.OAS3Options = {
+  definition: swaggerDefinition,
   // Paths to files containing OpenAPI definitions
-  apis: ["./src/notes/notes.routes.ts", "./src/notes/model/notes.interface.ts"],
+  apis: [
+    "./src/notes/notes.routes.ts",
+    "./src/notes/model/notes.interface.ts",
+    "./src/utils/swagger.components.ts",
+  ],
 };
 
 export const swaggerSpec = swagger(options);
